d77fc73aeb28dd71c36d910acdecbda0b6632615
Василий Чернов < >
2017-03-06 20:56:25 +0300

    Переработана генерация сигнала

--- signal_utils/generation_utils.py
+++ signal_utils/generation_utils.py
@@ -13,86 +13,38 @@ def generate_noise(size):
     c2 = np.convolve(c1, np.full((3), 1/3), 'valid')
     c3 = np.convolve(c2, np.full((3), 1/3), 'valid') + 2.5
     return np.round(c3)
-
-
-def gen_peak(ampl=1000, use_offset=True, p=1, 
-             sigma=0.6, n_bins=20, left=-2, right=2):
+    
+    
+def gen_signal(x, ampl, pos, 
+          sigma=0.34156352, 
+          tail_amp=0.37015974, 
+          tail_factor=2.96038975,
+          p=2.20563286,
+          s=0.0525933):
     """
-      Генерация пика сигнала
+      Генерацмя сигнала
       
-      В качестве пика используется форма [обобщенного гауссиана](https://docs.scipy.org/doc/scipy/reference/generated/scipy.signal.general_gaussian.html#scipy.signal.general_gaussian)
-      Все аргументы, кроме @ampl и @use_offset подобраны под реальный сигнал.
+      Все аргументы, кроме @ampl и @pos подобраны под реальный сигнал.
       Рекомендуется оставить из значения по умолчанию
       
-      @ampl - апплитуда пика в каналах
-      @use_offset - производить смещение пика в пределах бина. Смещение нужно для того, 
-      чтобы пик не попадал в бин, как в реальных данных
-      @p - параметр гауссиана
-      @sigma - параметр гауссиана
-      @n_bins - количество бинов 
-      @left - левая граница гауссиана 
-      @right - правая граница гауссиана
-      @return - массив с пиком
+      @x - координаты
+      @ampl - амплитуда генерируемого сигнала 
+      @pos - положение генерируемого сигнала
+      @sigma - растяжение сигнала
+      @tail_amp - отношение амлитуды выброса к амплитуде сигнала
+      @tail_factor - степень резкости пика сигнала
+      @p - степень гауссиана амплитуда
+      @s - коэффициент растяжения выброса
+      @return сигнал для заданных координат
       
     """
+
+    gauss = lambda x: np.exp((-1/2)*np.power((np.abs(sigma*x)), p))
+    spike = lambda x: (1/(1+2*x*s)**tail_factor - 1.0)*np.exp(-x*s)
     
-    offset = 0
-    if use_offset:
-        bin_error = (right - left)/n_bins
-        offset = np.random.uniform(-bin_error, bin_error)
+    y = gauss(x - pos)
+    spike_offset = np.where(y > 0.1)[0][-1]
+    y[spike_offset:] += spike(x[spike_offset:] - x[spike_offset])*tail_amp
         
-    off = lambda x: x + offset
-    gauss_gen = lambda x: np.exp((-1/2)*np.power((np.abs(x/sigma)), 2*p))
-    data = np.array([gauss_gen(off(x)) for x in np.linspace(-2, 2, 20)])
-    return data*ampl
-
-
-def generate_spike(ampl=400, a=-0.19009746026101096, 
-                   b=-56.524973694092296, lenght=42):
-    """
-      Генерация выброса сигнала
-      
-      Все аргументы, кроме @ampl подобраны под реальный сигнал.
-      Рекомендуется оставить из значения по умолчанию
-      
-      @ampl - апплитуда пика в каналах
-      @a - коэффициент соотношения амплитуды пика и выброса
-      @b - коэффициент соотношения амплитуды пика и выброса
-      @lenght - длина выброса
-      @return - массив с выбросом
-      
-    """
-    
-    spike_amp = np.abs(a*ampl + b)
-    
-    formula = lambda x, s: (1/(1+2*x*s)**3 - 0.9)*np.exp(-x*s) 
-    data = np.array([formula(x, 0.048) for x in np.linspace(0, 100, lenght*2)])
-    data = data/np.abs(data.min())*spike_amp
-    
-    return data
-
-
-def gen_signal(peak_amp, l_offset=45, add_noise=False):
-    """
-      Генерация сигнала
-      
-      @peak_amp - апплитуда пика в каналах
-      @l_offset - положение сигнала
-      @add_noise - Добавление шума
-      @return - сигнал
-      
-    """
-    peak = gen_peak(peak_amp)
-    spike = generate_spike(peak_amp)
-
-    signal = np.zeros(l_offset + len(peak) + len(spike))
-    signal[l_offset:l_offset + len(peak)] = peak
-    signal[l_offset + round(len(peak)*0.76): 
-           l_offset + round(len(peak)*0.76) + len(spike)] = spike
-
-    if add_noise:
-        noise = generate_noise(len(signal) - 2)
-        return signal + noise*16
-    else:
-        return signal
+    return y*ampl
 
