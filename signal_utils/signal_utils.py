4ca29cfa4abca11bdf2c07a724aec1454343b5f7
Василий Чернов < >
2017-03-31 19:46:26 +0300

    add zero-suppression algotitm

--- signal_utils/signal_utils.py
+++ signal_utils/signal_utils.py
@@ -9,6 +9,33 @@ from scipy.signal import argrelextrema
 from gdrive_utils import load_dataset, get_points_drom_drive
 from generation_utils import gen_multiple
 
+
+def apply_zsupression(data: np.ndarray, threshold: int=500, 
+                          area_l: int=50, area_r: int=100) -> tuple:
+    """
+      Обрезание шумов в файле данных платы Лан10-12PCI
+      
+      Функция расчитана на файлы данных с максимальным размером кадра
+      (непрерывное считывание с платы).
+      
+      @data - данные кадра (отдельный канал)
+      @threshold - порог амплитуды события
+      @area_l - область около события, которая будет сохранена
+      @area_r - область около события, которая будет сохранена
+      
+      @return список границ события
+      
+    """
+    peaks = np.where(data > threshold)[0]
+    dists = peaks[1:] - peaks[:-1]
+    gaps = np.append(np.array([0]), np.where(dists > area_r)[0] + 1)
+    
+    events = ((peaks[gaps[gap]] - area_l, peaks[gaps[gap + 1] - 1] + area_r) 
+              for gap in range(0, len(gaps) - 1))
+    
+    return events
+
+
 def calc_center(ev, amp_arg, step=2):
     """
       Параболическая аппроксимация центра пика
