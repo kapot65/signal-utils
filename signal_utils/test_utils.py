f9e4f745de84bf196b69fe68753a88b040bd90e7
Vasilii Chernov <kapot65@gmail.com>
2017-04-04 16:50:01 +0300

    isolate drawing in testing function

--- signal_utils/test_utils.py
+++ signal_utils/test_utils.py
@@ -1,12 +1,228 @@
 # -*- coding: utf-8 -*-
 
 from datetime import datetime
+from functools import partial
 
 import numpy as np
 import matplotlib.pyplot as plt
 
 from generation_utils import gen_raw_block
 from extract_utils import apply_zsupression
+
+
+def _extract_events(data, positions, ev_pre_l, ev_pre_r, sample_freq):
+    """
+      Вырезание кадров событий из данных
+      
+      @data - массив данных сигнала
+      @positions - массив положений пиков в наносекундах с начала кадра
+      @ev_pre_l - размер кадра сохраняемого события слева от пика
+      @ev_pre_r - размер кадра сохраняемого события справа от пика
+      @sample_freq
+      
+      @return - массив кадров событий
+    """
+    
+    frames_extr = np.zeros((len(positions), ev_pre_l + ev_pre_r), 
+                           np.float32)
+    
+    for i, pos in enumerate((positions*(sample_freq*1e-9)).astype(np.int)):
+        if 0 < pos - ev_pre_l and pos + ev_pre_r < data.shape[0]:
+            frames_extr[i] = data[pos - ev_pre_l: pos + ev_pre_r]
+            
+    return frames_extr
+    
+
+def _generate_block(b, algoritm_func, max_pos_err, 
+                    threshold, area_l, area_r, 
+                    freq, sample_freq, b_size, 
+                    min_amp, max_amp,
+                    ev_pre_l: int=10,
+                    ev_pre_r: int=50):
+    """
+      Генерация кадра и выделение из него событий.
+      
+      @b - номер блока
+      @algoritm_func - фукнция выделения
+      @max_pos_err - макисмальная ошибка выделения положения пика (нс)
+      @threshold - порог zero-suppression
+      @area_l - левая область zero-suppression
+      @area_r - правая область zero-suppression
+      @freq - скорость счета в секунду
+      @sample_freq - частота оцифровки тестовых данных 
+      @b_size - размер кадра
+      @min_amp - минимальная амлитуда генерируемого события 
+      @max_amp - максимальная амлитуда генерируемого события
+      @ev_pre_l - размер кадра сохраняемого события слева от пика
+      @ev_pre_r - размер кадра сохраняемого события справа от пика
+      
+      @return (amps_real, pos_real, frames_real, 
+      amps_extracted, pos_extracted, frames_extr, singles_extracted, 
+      delta)
+      amps_real, amps_extracted - массив реальных и восстановленных амплитуд
+      pos_real, pos_extracted - массив реальных и восстановленных положений 
+      пиков в наносекундах
+      frames_real, frames_extr - массив реальных и восстановленных кадров 
+      событий
+      singles_extracted - массив классов восстановленных событий
+      delta - время работы алгоритма выделения в секундах
+      
+    """
+    
+    block_off = b*((b_size/sample_freq)*1e+9 + 2*max_pos_err)
+    
+    data, params = gen_raw_block(freq, sample_freq, b_size, 
+                                 min_amp, max_amp)
+    blocks = np.array(list(apply_zsupression(data, threshold, 
+                                             area_l, area_r)))
+    times = (blocks[:,0]/sample_freq)*1e+9  #наносекунды
+    
+    params_extracted = []
+    singles_extracted = []
+    
+    time_start = datetime.now()
+    for i in range(len(blocks)):
+        block = data[blocks[i][0]:blocks[i][1]]
+        events, singles = algoritm_func(block, threshold, 
+                                        times[i], sample_freq)
+        params_extracted.append(events)
+        singles_extracted.append(singles)
+    delta = (datetime.now() - time_start).total_seconds()
+    
+    params_extracted = np.hstack(params_extracted)
+    
+    amps_real = params[0::2]
+    pos_real = (params[1::2]/sample_freq)*1e+9
+    frames_real = _extract_events(data, pos_real, 
+                                  ev_pre_l, ev_pre_r, 
+                                  sample_freq)
+    
+    amps_extracted = params_extracted[0::2]
+    pos_extracted = params_extracted[1::2] 
+    
+    frames_extr = _extract_events(data, pos_extracted, 
+                                  ev_pre_l, ev_pre_r, 
+                                  sample_freq)
+    
+    singles_extracted = np.hstack(singles_extracted)
+    
+    return amps_real, pos_real + block_off, frames_real,\
+           amps_extracted,  pos_extracted + block_off, frames_extr,\
+           singles_extracted, delta
+           
+
+def _calc_metrics(amps_real, pos_real, frames_real, 
+                 amps_extracted, pos_extracted, frames_extracted, 
+                 singles_extracted, delta, max_pos_err):
+    """
+      Вычисление метрик
+      
+    """
+    metrics = {}
+    metrics["time_elapsed"] = delta
+    metrics["total_real"] = len(amps_real)
+    metrics["total_detected"] = len(amps_extracted)
+
+    
+    idxs_raw = np.abs(np.subtract.outer(pos_extracted, pos_real)).argmin(0)
+    dists = np.abs(pos_real - pos_extracted[idxs_raw])
+    idxs_raw[dists > max_pos_err] = -1
+    single_idxs, counts = np.unique(idxs_raw, return_counts=True)
+    
+    metrics["real_detected_transitions"] = idxs_raw
+    metrics["false_negatives"] = np.arange(len(idxs_raw))[idxs_raw==-1]
+    metrics["false_positives"] = np.setdiff1d(np.arange(len(pos_extracted)), \
+                                              single_idxs)
+    
+    doubles_real = single_idxs[counts > 1]
+    doubles_real = doubles_real[doubles_real != -1]
+    metrics["doubles_real"] = doubles_real
+    metrics["doubles_detected"] = np.arange(len(singles_extracted))\
+                                  [np.where(singles_extracted==False)]
+                                  
+    return metrics
+           
+          
+def draw_metrics(metrics: dict):
+    """
+      Отрисовка метрик
+      
+    """
+    print("Summary: ")
+    print("Algoritm time: %s s"%(metrics["time_elapsed"]))
+    print("Total events: %s"%(metrics["total_real"]))
+    print("Detected events: %s"%(metrics["total_detected"]))
+    print("Positives/negatives: ")
+    print("False negatives %s"%(len(metrics["false_negatives"])))
+    print("False positives %s"%(len(metrics["false_positives"])))
+    
+    idxs_raw = metrics["real_detected_transitions"]
+    
+    def get_dbl_amps_dists(idxs):
+        
+        dists, amps = [], []
+        
+        for idx in idxs:
+            doubles_pos = metrics["pos_real"][idxs_raw == idx]
+            doubles_amps = metrics["amps_real"][idxs_raw == idx]
+            dists.append(doubles_pos[1:] - doubles_pos[:-1])
+            amps.append(np.log(doubles_amps[1:], 
+                               doubles_amps[:-1]))
+        
+        if len(dists):
+            dists = np.hstack(dists)
+            amps = np.hstack(amps)
+            return amps, dists
+        else:
+            return np.array([]), np.array([])
+    
+    dbl_amps_real, dbl_dists_real = get_dbl_amps_dists(metrics["doubles_real"])
+    dbl_amps_det, \
+    dbl_dists_det = get_dbl_amps_dists(metrics["doubles_detected"])
+    
+    fig, ax = plt.subplots(3, 2)
+    hist, bins = np.histogram(dbl_dists_real, 40)
+    hist_det, bins = np.histogram(dbl_dists_det, bins=bins)
+    ax[2][0].set_title("real doubles")
+    ax[2][0].hist2d(dbl_dists_real, dbl_amps_real)
+    ax[2][1].set_title("detected doubles")
+    ax[2][1].hist2d(dbl_dists_det, dbl_amps_det)
+    ax[0][0].set_title("real/detected doubles dists cumsums")
+    ax[0][0].plot(bins[1:], np.cumsum(hist), label='real')
+    ax[0][0].plot(bins[1:], np.cumsum(hist_det), label='detected')
+    ax[0][0].legend()
+    
+    print("%s detected \n"\
+          "%s real \n"\
+          "%s intersection"%(len(metrics["doubles_detected"]),
+                             len(metrics["doubles_real"]),
+                             len(np.intersect1d(metrics["doubles_detected"],
+                                                metrics["doubles_real"]))))
+    
+    print("Amplitude accuracy:")
+    error = metrics["amps_real"][idxs_raw != -1] - \
+            metrics["amps_extracted"][idxs_raw[idxs_raw != -1]]
+            
+    ax[0][1].set_title("amplitude error")
+    ax[0][1].hist(error, 40)
+    
+    ax[1][1].set_title("negative_amplitudes error")
+    ax[1][1].hist(metrics["amps_real"][np.where(idxs_raw == -1)[0]], 40)
+    
+    
+    idxs_raw[idxs_raw==-1]
+    
+    range_ = (min(metrics["amps_real"].min(), 
+                  metrics["amps_extracted"].min()),
+              max(metrics["amps_real"].max(), 
+                  metrics["amps_extracted"].max()))
+    
+    ax[1][0].set_title("amp hists")
+    ax[1][0].hist(metrics["amps_real"], 80, \
+                  fc=(1,0,0,0.5), label="real", range=range_)
+    ax[1][0].hist(metrics["amps_extracted"], 80, fc=(0,0,1,0.5),
+            label="extracted", range=range_)
+    ax[1][0].legend()
     
 
 def test_algoritm(algoritm_func,
@@ -17,6 +233,7 @@ def test_algoritm(algoritm_func,
                   freq: float=12e+3, 
                   sample_freq: float=3125000.0, 
                   b_size: int=1048576,
+                  n_blocks: int=1,
                   min_amp: int=700, 
                   max_amp: int=7000):
     """
@@ -85,141 +302,40 @@ def test_algoritm(algoritm_func,
       @freq - скорость счета в секунду
       @sample_freq - частота оцифровки тестовых данных 
       @b_size - размер кадра
+      @n_blocks - количество кадров
       @min_amp - минимальная амлитуда генерируемого события 
       @max_amp - максимальная амлитуда генерируемого события 
       
     """
-    global data
-    global pos_extracted
-    global singles_detected
-    global singles_real
-
-    amps_real = np.array([])
-    pos_real = np.array([])
     
-    amps_extracted = np.array([])
-    pos_extracted = np.array([])
+    func = partial(_generate_block, algoritm_func=algoritm_func, 
+                   max_pos_err=max_pos_err, threshold=threshold, area_l=area_l, 
+                   area_r=area_r, freq=freq, sample_freq=sample_freq, 
+                   b_size=b_size, min_amp=min_amp, max_amp=max_amp)
     
-    data, params = gen_raw_block(freq, sample_freq, b_size, 
-                                 min_amp, max_amp)
-    blocks = np.array(list(apply_zsupression(data, threshold, 
-                                             area_l, area_r)))
-    times = (blocks[:,0]/sample_freq)*1e+9  #наносекунды
+    events = np.array([func(b) for b in range(n_blocks)])
     
-    params_extracted = []
-    singles_extracted = []
+    amps_real = np.hstack(events[:, 0])
+    pos_real = np.hstack(events[:, 1])
+    frames_real = np.vstack(events[:, 2])
+    amps_extracted = np.hstack(events[:, 3])
+    pos_extracted = np.hstack(events[:, 4])
+    frames_extracted = np.vstack(events[:, 5])
+    singles_extracted = np.hstack(events[:, 6])
+    delta = events[:, 7].sum()
     
-    time_start = datetime.now()
-    for i in range(len(blocks)):
-        block = data[blocks[i][0]:blocks[i][1]]
-        events, singles = algoritm_func(block, threshold, 
-                                        times[i], sample_freq)
-        params_extracted.append(events)
-        singles_extracted.append(singles)
-    delta = (datetime.now() - time_start).total_seconds()
+    metrics = _calc_metrics(amps_real, pos_real, frames_real, 
+                           amps_extracted, pos_extracted, frames_extracted, 
+                           singles_extracted, delta, max_pos_err)
     
-    params_extracted = np.hstack(params_extracted)
-    singles_extracted = np.hstack(singles_extracted)
-    
-    amps_real = np.hstack([amps_real, params[0::2]])
-    pos_real = np.hstack([pos_real,(params[1::2]/sample_freq)*1e+9])
-    
-    amps_extracted = np.hstack([amps_extracted, params_extracted[0::2]])
-    pos_extracted = np.hstack([pos_extracted, params_extracted[1::2]])
-    
-    # Анализ
-    print("Summary: ")
-    print("algoritm time: %s s"%(delta))
-    print("total events: %s"%(len(amps_real)))
-    print("detected events: %s"%(len(amps_extracted)))
-    
-    idxs_raw = np.abs(np.subtract.outer(pos_extracted, pos_real)).argmin(0)
-    dists = np.abs(pos_real - pos_extracted[idxs_raw])
-    idxs_raw[dists > max_pos_err] = -1
-    single_idxs, counts = np.unique(idxs_raw, return_counts=True)
-    
-    print("Positives/negatives: ")
-    false_neg = len(idxs_raw[idxs_raw==-1])
-    print("false negatives %s (%s)"%(false_neg, false_neg/len(idxs_raw)))
-    
-    false_pos = len(np.setdiff1d(np.arange(len(pos_extracted)), single_idxs))
-    print("false positives %s (%s)"%(false_pos, false_pos/len(pos_real)))
-    
-    print("Singles/doubles: ")
-    singles_detected = np.arange(len(singles_extracted))\
-    [np.where(singles_extracted==False)]
-    singles_real = single_idxs[counts > 1]
-    singles_real = singles_real[singles_real != -1]
-    
-    singles_real_dists = []
-    singles_real_amps = []
-    for idx in singles_real:
-        doubles_real_pos = pos_real[idxs_raw == idx]
-        doubles_real_amps = amps_real[idxs_raw == idx]
-        singles_real_dists.append(doubles_real_pos[1:] - doubles_real_pos[:-1])
-        singles_real_amps.append(np.log(doubles_real_amps[1:]/
-                                        doubles_real_amps[:-1]))
-        
-        
-    singles_real_dists = np.hstack(singles_real_dists)
-    singles_real_amps = np.hstack(singles_real_amps)
-    
-    singles_det_dists = []
-    singles_det_amps = []
-    for idx in singles_detected:
-        doubles_real_pos = pos_real[idxs_raw == idx]
-        doubles_real_amps = amps_real[idxs_raw == idx]
-        if len(doubles_real_amps) > 1:
-            singles_det_dists.append(doubles_real_pos[1:] - 
-                                     doubles_real_pos[:-1])
-            singles_det_amps.append(np.log(doubles_real_amps[1:]/
-                                            doubles_real_amps[:-1]))
-    
-    if len(singles_det_dists):
-        singles_det_dists = np.hstack(singles_det_dists)
-        singles_det_amps = np.hstack(singles_det_amps)
-    
-    
-    fig, ax = plt.subplots(3, 2)
-    hist, bins = np.histogram(singles_real_dists, 40)
-    hist_det, bins = np.histogram(singles_det_dists, bins=bins)
-    ax[2][0].set_title("real doubles")
-    ax[2][0].hist2d(singles_real_dists, singles_real_amps)
-    ax[2][1].set_title("detected doubles")
-    ax[2][1].hist2d(singles_det_dists, singles_det_amps)
-    ax[0][0].set_title("real/detected doubles dists cumsums")
-    ax[0][0].plot(bins[1:], np.cumsum(hist), label='real')
-    ax[0][0].plot(bins[1:], np.cumsum(hist_det), label='detected')
-    ax[0][0].legend()
-    
-    print("%s detected \n"\
-          "%s real \n"\
-          "%s intersection"%(len(singles_detected),
-                             len(singles_real),
-                             len(np.intersect1d(singles_detected,
-                                                singles_real))))
-    
-    print("Amplitude accuracy:")
-    error = amps_real[idxs_raw != -1] - \
-            amps_extracted[idxs_raw[idxs_raw != -1]]
-            
-    ax[0][1].set_title("amplitude error")
-    ax[0][1].hist(error, 40)
-    
-    ax[1][1].set_title("negative_amplitudes error")
-    ax[1][1].hist(amps_real[np.where(idxs_raw == -1)[0]], 40)
-    
-    
-    idxs_raw[idxs_raw==-1]
-    
-    range_ = (min(amps_real.min(), amps_extracted.min()),
-              max(amps_real.max(), amps_extracted.max()))
-    
-    ax[1][0].set_title("amp hists")
-    ax[1][0].hist(amps_real, 80, fc=(1,0,0,0.5), label="real", range=range_)
-    ax[1][0].hist(amps_extracted, 80, fc=(0,0,1,0.5),
-            label="extracted", range=range_)
-    ax[1][0].legend()
+    return {"amps_real": amps_real,
+            "pos_real": pos_real, 
+            "frames_real": frames_real,     
+            "amps_extracted": amps_extracted, 
+            "pos_extracted": pos_extracted, 
+            "frames_extracted": frames_extracted,         
+            "singles_extracted": singles_extracted, 
+            **metrics}
     
 
 if __name__ == '__main__':
@@ -227,4 +343,6 @@ if __name__ == '__main__':
     from pylab import rcParams
     from extract_utils import extract_fit_all
     rcParams['figure.figsize'] = 10, 10
-    test_algoritm(extract_fit_all, b_size=1048576*2)
+    res = test_algoritm(extract_fit_all, b_size=int(1048576), n_blocks=1)
+    draw_metrics(res)
+    
