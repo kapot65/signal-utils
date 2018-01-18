10f564d37172b9cc5549e8e7a8bd4637fc5919db
Vasilii Chernov <kapot65@gmail.com>
2018-01-18 13:28:30 +0300

    version hotfix

--- signal_utils/__init__.py
+++ signal_utils/__init__.py
@@ -1,20 +1,7 @@
 """Signal utils init script."""
-import sys
-from os import path
-
 from pkg_resources import get_distribution
 
-import convert_utils
-import draw_utils
-import extract_utils
-import gdrive_utils
-import generation_utils
-import test_utils
+from . import (convert_utils, draw_utils, extract_utils, gdrive_utils,
+               generation_utils, test_utils)
 
-MAIN_DIR = path.abspath(path.dirname(__file__))
-if MAIN_DIR not in sys.path:
-    sys.path.append(MAIN_DIR)
-del MAIN_DIR
-
-
-__version__ = get_distribution('dfparser').version
+__version__ = get_distribution('signal-utils').version
