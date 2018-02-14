fa1ba6b6a17fa999bb03b015967d1262b4fd0577
Vasilii Chernov <kapot65@gmail.com>
2018-02-14 13:13:38 +0300

    Add multiple file support to draw_slow.py

--- scripts/analysis/draw_slow.py
+++ scripts/analysis/draw_slow.py
@@ -9,14 +9,14 @@ import csv
 import io
 import re
 from argparse import ArgumentParser, ArgumentTypeError
+from glob import glob
 from os import path
 
 import dateutil
 import dfparser  # Numass files parser
-from dateutil.parser import parse as dateparse  # timestamp parser
-
 import matplotlib.pyplot as plt  # plotting library
 import seaborn as sns  # matplotlib grahs visual enchancer
+from dateutil.parser import parse as dateparse  # timestamp parser
 
 
 def parse_args():
@@ -57,48 +57,55 @@ def parse_args():
 def _main():
     args = parse_args()
     # Read dataforge point
-    header, _, data = dfparser.parse_from_file(args.input)  # Meta wont be used
-
-    # Read binay data manually due to machine header error (in 2017_10 set)
-    # Dont need for good data
-    header_len = dfparser.type_codes.ENVELOPE_HEADER_CODES[
-        header["type"]]["header_len"]
-
-    with (open(args.input, 'rb')) as raw_file:
-        raw_file.seek(header_len + header['meta_len'])
-        data = raw_file.read()
-
-    # Prettify data. Remove double spaces
-    data_tabed = re.sub(b'[ \t]+', b'\t', data)  # .replace(b'@null',  b'0')
-    # Remove icorrect column name '#f\t' from data
-    data_tabed = data_tabed[3:]
-
-    # Parse data as TSV format
-    tsv = csv.DictReader(io.StringIO(
-        data_tabed.decode()), delimiter='\t')
-
-    # Extracting values from parsed TSV
-    graphs = {}
-    for row in tsv:
-        for key in row.keys():
-            if key != 'timestamp':
-                if row[key] != '@null':  # Filter null values
-                    if key not in graphs:
-                        graphs[key] = {'x': [], 'y': []}
-
-                    # Append timestamp to point
-                    graphs[key]['x'].append(
-                        dateutil.parser.parse(row['timestamp']))
-                    # Append value to point
-                    graphs[key]['y'].append(
-                        float(row[key]))
 
     # Creating graph
     _, axes = plt.subplots()
-    # Plotting graphs
-    for graph in graphs.keys():
-        if graph not in args.exclude:  # Filter excluded data
-            axes.plot(graphs[graph]['x'], graphs[graph]['y'], label=graph)
+
+    for idx_f, input_file in enumerate(glob(args.input)):
+        header, _, data = dfparser.parse_from_file(input_file)
+        # Read binary data manually due to machine header error
+        # Dont need for good data
+        header_len = dfparser.type_codes.ENVELOPE_HEADER_CODES[
+            header["type"]]["header_len"]
+
+        with (open(input_file, 'rb')) as raw_file:
+            raw_file.seek(header_len + header['meta_len'])
+            data = raw_file.read()
+
+        # Prettify data. Remove double spaces
+        data_tabed = re.sub(b'[ \t]+', b'\t', data)
+        # Remove icorrect column name '#f\t' from data
+        data_tabed = data_tabed[3:]
+
+        # Parse data as TSV format
+        tsv = csv.DictReader(io.StringIO(
+            data_tabed.decode()), delimiter='\t')
+
+        # Extracting values from parsed TSV
+        graphs = {}
+        for row in tsv:
+            for key in row.keys():
+                if key != 'timestamp':
+                    if row[key] != '@null':  # Filter null values
+                        if key not in graphs:
+                            graphs[key] = {'x': [], 'y': []}
+
+                        # Append timestamp to point
+                        graphs[key]['x'].append(
+                            dateutil.parser.parse(row['timestamp']))
+                        # Append value to point
+                        graphs[key]['y'].append(
+                            float(row[key]))
+
+        # Plotting graphs
+        for idx, graph in enumerate(graphs.keys()):
+            if graph not in args.exclude:  # Filter excluded data
+                label = None
+                if idx_f == 1:
+                    label = graph
+                axes.plot(
+                    graphs[graph]['x'], graphs[graph]['y'],
+                    color=sns.color_palette()[idx], label=label)
 
     # Applying parameters
     if args.title:
