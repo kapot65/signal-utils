3df5e2f31859a00c4f9fd34e731008a8d010be11
chernov <kapot65@gmail.com>
2018-05-10 13:12:15 +0300

    update setup.py to pip 10

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 """Install script."""
 
-from pip.req import parse_requirements
 from setuptools import find_packages, setup
 
-INSTALL_REQS = parse_requirements("signal_utils/requirements.txt",
-                                  session='hack')
-REQS = [str(ir.req) for ir in INSTALL_REQS]
+
+def parse_requirements(filename):
+    """ load requirements from a pip requirements file """
+    lineiter = (line.strip() for line in open(filename))
+    return [line for line in lineiter if line and not line.startswith("#")]
 
 setup(
     name="signal_utils",
@@ -14,7 +15,7 @@ setup(
     author="Vasiliy Chernov",
     packages=find_packages(),
     platforms='any',
-    install_requires=REQS,
+    install_requires=parse_requirements("signal_utils/requirements.txt"),
     include_package_data=True,
     package_data={
         '': ['*.h5', '*.dat'],
