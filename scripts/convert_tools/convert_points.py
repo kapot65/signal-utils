32861f38648d600a3129ab42a47dcb2a672aa3ca
chernov <kapot65@gmail.com>
2018-09-27 15:13:27 +0300

    Add new exception to convert script

--- scripts/convert_tools/convert_points.py
+++ scripts/convert_tools/convert_points.py
@@ -2,16 +2,17 @@
 """Скрипт для конвертирования фреймов в события в df файлах."""
 # pylint: disable-msg=E0611
 
-from argparse import ArgumentParser
-from glob import glob
-from os import makedirs
-from os import path
-import sys
 import struct
-
+import sys
+from argparse import ArgumentParser
 from contextlib import closing
+from glob import glob
+from os import makedirs, path
+
 import dfparser
 from multiprocess import Pool
+from signal_utils.convert_utils import df_frames_to_events
+from signal_utils.extract_utils import extract_amps_approx
 from tqdm import tqdm
 
 MAIN_DIR = path.abspath(path.join(path.dirname(__file__), '..'))
@@ -19,11 +20,8 @@ if MAIN_DIR not in sys.path:
     sys.path.append(MAIN_DIR)
 del MAIN_DIR
 
-from signal_utils.convert_utils import df_frames_to_events
-from signal_utils.extract_utils import extract_amps_approx
 
-
-def parse_args():
+def _parse_args():
     """Парсинг параметров."""
     parser = ArgumentParser(description='Dataforge Lan10-12PCI frames to '
                             'events converter.')
@@ -37,7 +35,7 @@ def parse_args():
 
 
 if __name__ == "__main__":
-    ARGS = parse_args()
+    ARGS = _parse_args()
 
     FILES_ABS_WILDCARD = path.join(ARGS.data_path, ARGS.wildcard)
     FILES = glob(FILES_ABS_WILDCARD, recursive=True)
@@ -59,7 +57,8 @@ if __name__ == "__main__":
                 out_file.write(dfparser.create_message(meta_real_, data_real_))
         except struct.error:
             pass
-
+        except NotImplementedError:
+            pass
 
     with closing(Pool()) as pool:
         with tqdm(total=len(FILES)) as pbar:
